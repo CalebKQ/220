76955ec757ebd1c9a8f777a407cf46ffbb81bf54
QuirtleNom <81350544+QuirtleNom@users.noreply.github.com>
2021-08-30 16:38:09 -0400

    completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,13 @@
+"""
+Name: Caleb Quattlebaum
+hello.py
+
+Problem: this program is being used to test our environments by printing "hello, world!" on screen
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+def main():
+    print("hello, world!")
+    
